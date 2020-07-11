60d12f7b00306412d785197bf5f8db1be7245ff1
Anatoly Brevnov <tbdapplications@gmail.com>
2020-07-11 15:55:35 -0400

    Added Game Metadata API Endpoints

--- Backend API/Game Service/GameService/src/main/java/com/weflop/REST/RESTController.java
+++ Backend API/Game Service/GameService/src/main/java/com/weflop/REST/RESTController.java
@@ -1,10 +1,13 @@
 package com.weflop.REST;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.weflop.Game.Game;
+import com.weflop.Game.GameCustomMetadata;
 import com.weflop.Game.GameFactory;
 
 @Controller
@@ -13,8 +16,23 @@ public class RESTController {
 	@PostMapping("/create-game")
 	@ResponseBody
 	public String createGame(@RequestParam(name="user_id", required=true) String userId,
-			@RequestParam(name="small_blind", required=true, defaultValue="0.0") float smallBlind,
-			@RequestParam(name="game_type", required=false, defaultValue="0") int gameType) {
+			@RequestParam(name="small_blind", required=true) float smallBlind) {
+		
+		if (smallBlind < 0.5) {
+			throw new IllegalArgumentException("Small blind must be greater than or equal to 0.5");
+		}
+		
 		return GameFactory.generateStandardPokerGame(userId, smallBlind); // returning id of newly created game
+	}
+	
+	@GetMapping("/game-metadata")
+	@ResponseBody
+	public GameCustomMetadata getGameMetadata(@RequestParam(name="game_id", required=true) String gameId) {
+		Game game = GameFactory.ID_TO_GAME.get(gameId);
+		if (game == null) {
+			throw new IllegalArgumentException("No game exists with id: " + gameId);
+		}
+		
+		return game.getGameMetadata();
 	}
 }
